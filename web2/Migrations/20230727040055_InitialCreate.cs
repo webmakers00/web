78615cd3bb5a6dc7a7a2e533ec26078a17f0d9da
webmakers00 <469260979@qq.com>
2023-07-27 12:07:45 +0800

    aaaaa

---
+++ web2/Migrations/20230727040055_InitialCreate.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web2.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ord",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ordid = table.Column<int>(type: "INTEGER", nullable: false),
+                    cusna = table.Column<string>(type: "TEXT", nullable: true),
+                    cusid = table.Column<int>(type: "INTEGER", nullable: false),
+                    n = table.Column<int>(type: "INTEGER", nullable: false),
+                    dstar = table.Column<int>(type: "INTEGER", nullable: false),
+                    dend = table.Column<int>(type: "INTEGER", nullable: false),
+                    first = table.Column<int>(type: "INTEGER", nullable: false),
+                    des = table.Column<string>(type: "TEXT", nullable: true),
+                    isdel = table.Column<int>(type: "INTEGER", nullable: false),
+                    isend = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ord", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "step",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    isstar = table.Column<bool>(type: "INTEGER", nullable: false),
+                    isend = table.Column<bool>(type: "INTEGER", nullable: false),
+                    dstar = table.Column<int>(type: "INTEGER", nullable: false),
+                    dend = table.Column<int>(type: "INTEGER", nullable: false),
+                    month = table.Column<int>(type: "INTEGER", nullable: false),
+                    ordId = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_step", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_step_ord_ordId",
+                        column: x => x.ordId,
+                        principalTable: "ord",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "boy",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    n = table.Column<int>(type: "INTEGER", nullable: false),
+                    allmoney = table.Column<int>(type: "INTEGER", nullable: false),
+                    stepId = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_boy", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_boy_step_stepId",
+                        column: x => x.stepId,
+                        principalTable: "step",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_boy_stepId",
+                table: "boy",
+                column: "stepId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_step_ordId",
+                table: "step",
+                column: "ordId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "boy");
+
+            migrationBuilder.DropTable(
+                name: "step");
+
+            migrationBuilder.DropTable(
+                name: "ord");
+        }
+    }
+}
--- web2/ef/ord.cs
+++ web2/ef/ord.cs
@@ -1,25 +1,45 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 //Scaffold-DbContext "Data Source=d:\web\mgf.db" Microsoft.EntityFrameworkCore.Sqlite -OutputDir Models
 namespace web2.ef
 {
     public class ord
     {
-        [Key]        
-        public long Id { get; set; }
-        public int myid { get; set; }
-        public List<stre> ords { get; set;}
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // 自增主键       
+        public long Id { get; set; } //自动增加
+        public int ordid { get; set; } //如“4052”
+        public string? cusna { get; set; } //如 “李天虎”
+        public int cusid { get; set; } //如 “ltf”
+        public int n { get; set; } //m套
+        public int dstar { get; set; }  //下单日期
+        public int dend { get; set; }// 出货日期
+        public int first { get; set; }//  加急到数天九
+        public string? des { get; set; } // 备注
+        public int isdel { get; set; } // 订单作废，如重复
+        public int isend { get; set; } // 订单出货
+        public List<step> istep { get; set; } = new List<step>(); //工序
     }
-    public class st
-    {
-        public long Id { get; set; } = 0;
-        public int myid { get; set; }
-
+    public class step
+    {   
+        public string name { get; set; } //工序名 “木工”
+        public bool isstar { get; set; } //是否开始
+        public bool isend { get; set; } //是否结束
+        public int dstar { get; set; } //开始时间
+        public int dend { get; set; } //结束时间
+        public int month { get; set; } //工序工钱计算月份
+        public List <boy> iboy { get; set;}=new List<boy>();    
     }
-
-
+    public class boy
+    {                  
+        public string name { get; set; } //员工名字  
+        public int n { get; set; } //自动增加  
+        public int allmoney { get; set; } //自动增加  
+    }
     public class ordcng : IEntityTypeConfiguration<ord>
     {
         public void Configure(EntityTypeBuilder<ord> builder)
@@ -27,13 +47,26 @@ namespace web2.ef
             builder.ToTable(nameof(ord));
         }
     }
+    public class boycnf : IEntityTypeConfiguration<boy>
+    {
+        public void Configure(EntityTypeBuilder<boy> builder)
+        {
+            builder.ToTable(nameof(boy));
+        }
+    }
+      public class stepcng : IEntityTypeConfiguration<step>
+    {
+        public void Configure(EntityTypeBuilder<step> builder)
+        {
+            builder.ToTable(nameof(step));
+        }
+    }
 
-  
-
-    
     public class Mydbcontext:DbContext
     {
         public DbSet<ord> ords { get; set;}
+        public DbSet<boy> boys { get; set;}
+        public DbSet<step> steps { get; set;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
@@ -51,3 +84,7 @@ namespace web2.ef
 
 
 }
+
+
+//Add-Migration InitialCreate
+//Update-Database
