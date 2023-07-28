dcf9e8ddf0f71f4dcd6b4a2d206698deb0d121d9
main <469260979@qq.com>
2023-07-28 13:26:31 +0800

    111

--- myunit/pages/index/index.vue
+++ myunit/pages/index/index.vue
@@ -1,9 +1,13 @@
 <template>
 	<view class="container">
-		
+		<scroll-view scroll-x="true" class="ctab" >
+			<view v-for="(item,index) in dtab" :key="index" class="ctabidx">
+				<text>{{item.name}}</text>
+			</view>
+		</scroll-view>
 		<!-- <view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
 		<text class="intro">详见：</text>
-		 --><uni-link :href="href" :text="href"></uni-link>
+		 <uni-link :href="href" :text="href"></uni-link> -->
 	</view>
 </template>
 
@@ -13,7 +17,16 @@
 	export default {
 		data() {
 			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui',
+				dtab:[
+					{name:"进度概略"},
+					{name:"客户查询"},
+					{name:"单号查询"},
+					{name:"型号查询"},
+					{name:"信息录入"},
+					{name:"订单修改"}
+					
+				]
 			}
 		},
 		onLoad() {
@@ -21,14 +34,8 @@
 		},
 		methods: {
         _init(){
-		// console.log($http.c);
-		$http.req({
-			url="\alist"
-		}).then(()=>{
-			
-		}).catch(()=>{
-			
-		})
+		  console.log($http.c);
+		
 		
 		
 		}
@@ -37,9 +44,18 @@
 </script>
 
 <style>
-	.container {
+	.container1 {
 		padding: 20px;
 		font-size: 14px;
 		line-height: 24px;
+	}
+    .ctab{
+		width: 100%;
+		height: 80rpm;
+		white-space: nowrap;
+	}
+	.ctabidx{
+		display: inline-block;
+		padding: 10rpx 20rpx;
 	}
 </style>
