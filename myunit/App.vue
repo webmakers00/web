2b36a4b330d2a8f7f72eded36bf429cd2345068c
webmakers00 <469260979@qq.com>
2023-08-05 18:31:40 +0800

    aaa

--- myunit/App.vue
+++ myunit/App.vue
@@ -15,7 +15,8 @@
 
 <style lang="scss">
 	/*每个页面公共css */
-	@import '@/uni_modules/uni-scss/index.scss';
+	@import './comm/pub.css';
+	@import './uni_modules/uni-scss/index.scss';
 	/* #ifndef APP-NVUE */
 	@import '@/static/customicons.css';
 	// 设置整个项目的背景色
--- myunit/pages/index/index.vue
+++ myunit/pages/index/index.vue
@@ -1,10 +1,23 @@
 <template>
 	<view class="container">
-		<scroll-view scroll-x="true" class="ctab" >
-			<view v-for="(item,index) in dtab" :key="index" class="ctabidx">
-				<text>{{item.name}}</text>
+		<scroll-view scroll-x="true"  class="ctab" >
+			<view v-for="(item,index) in dtab" :key="index"  @tap='tabclick(index)' class="ctabidx">
+				<!-- <text>{{item.name}}</text> -->
+				<text :class='tabidx===index ? "f-active-color" : "f-color"'>{{item.name}}</text>
 			</view>
-		</scroll-view>
+		</scroll-view>		
+		<swiper  :indicator-dots="true" :autoplay="false" :interval="3000" :duration="1000"  @change="tabchange" :current="tabidx">
+			<swiper-item v-for="(item,index) in dtab" :key="index" >
+				<view class="swiper-item">
+					{{item.name}}
+				</view>
+			</swiper-item>
+			
+		</swiper>
+		<!-- <uni-swiper-dot :info="" :current="" field="" :mode=""></uni-swiper-dot>
+		<switch checked="true" @change="" /> -->
+		
+		
 		<!-- <view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
 		<text class="intro">详见：</text>
 		 <uni-link :href="href" :text="href"></uni-link> -->
@@ -18,6 +31,7 @@
 		data() {
 			return {
 				href: 'https://uniapp.dcloud.io/component/README?id=uniui',
+				tabidx:0,
 				dtab:[
 					{name:"进度概略"},
 					{name:"客户查询"},
@@ -34,10 +48,17 @@
 		},
 		methods: {
         _init(){
-		  console.log($http.c);
-		
-		
-		
+		  console.log($http.c);	
+		},
+		tabclick(index){
+			if (index===this.tabidx) {	return;}
+			this.tabidx=index;
+			console.log(this.tabidx);
+		},
+		tabchange(e){
+			this.tabidx=e.detail.current;
+			console.log(e.detail.current);
+			console.log(e.detail.current+100);
 		}
 		}
 	}
